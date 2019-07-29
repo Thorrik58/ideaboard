1f8883a575343fd248b30f123d3910922e866138
thorrik58 <thorrik58@gmail.com>
2019-07-29 09:15:00 +0000

    basic add and remove functionality implemented.

--- components/IdeaCard/index.js
+++ components/IdeaCard/index.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types'
 
 const IdeaCard = ({ id, title, body, creationDate, onClick }) => {
   return (
-    <div className={ styles.card } onClick={()=>{ onClick(title, id, body, creationDate) }}>
+    <div className={ styles.card } onClick={()=>{ onClick(id, title, creationDate, body, 'delete') }}>
       <p>{title}</p>
       <p>{id}</p>
       <p>{body}</p>
@@ -13,7 +13,7 @@ const IdeaCard = ({ id, title, body, creationDate, onClick }) => {
   }
 
 IdeaCard.propTypes = {
-  id: PropTypes.string.isRequired,
+  id: PropTypes.number.isRequired,
   title: PropTypes.string.isRequired,
   body: PropTypes.string.isRequired,
   creationDate: PropTypes.string.isRequired,
--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,36 @@
 import styles from "./styles.scss"
 import React, {Component} from 'react'
-// import PropTypes from 'prop-types'
+import PropTypes from 'prop-types'
 import IdeaCard from '../components/IdeaCard'
+import fetch from 'isomorphic-unfetch'
 
-class HelloUA extends Component {
-  // static async getInitialProps({ req }) {
-  //   const userAgent = req ? req.headers['user-agent'] :navigator.userAgent
-  //   return { userAgent }
-  // }
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      cards: this.props.data,
+      additionalIdeasCount: 0 
+    }
+  }
+
+  static async getInitialProps() {
+    const res = await fetch('http://localhost:4000/ideas')
+    const data = await res.json();
+    return { data }
+  }
 
   // Should handle all edits and removal of cards
   onClickCard(id, title, creationDate, body, clickType) {
     if (clickType === 'delete') {
-      // eslint-disable-next-line no-console
-      console.log('delete '+ id + ' ' + title + ' ' + creationDate + ' ' + body)
+      fetch('http://localhost:4000/ideas/'+id, {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      }).then( r => {
+        console.log(r.status)
+      })
     }
     if (clickType === 'edit') {
       // eslint-disable-next-line no-console
@@ -25,17 +42,39 @@ class HelloUA extends Component {
     }
   }
 
+  onClickAdd() {
+    this.setState({cards: this.state.cards.concat({title: "Title", body: "Temporary body", created_date: new Date().getTime(), id: 123})
+  })
+    // fetch('http://localhost:4000/ideas', {
+    //   method: 'POST',
+    //   headers: {
+    //     'Content-Type': 'application/json'
+    //   },
+    //   body: JSON.stringify({ title: 'test', body: 'some more text', created_date: new Date().getTime()})
+    // }).then( r => {
+    //   console.log(r.status)
+    // })
+  }
+
   render() {
     return (
-    <div className={ styles.example }>
-      <IdeaCard title={'title'} body={'body'} id={'1'} creationDate={'2019something'} onClick={this.onClickCard}/>
-    </div>
+      <div className={ styles.cardContainer }>
+        {this.state.cards.map((idea, key) => 
+          <IdeaCard 
+            key={key}
+            id={idea.id}
+            title={idea.title}
+            body={idea.body}
+            creationDate={idea.created_date}
+            onClick={this.onClickCard}/>)}
+        <button onClick={() => { this.onClickAdd()}}>Add New Idea</button>
+      </div>
     )
   }
 }
 
-// HelloUA.propTypes = {
-//   userAgent: PropTypes.object.isRequired
-// }
+App.propTypes = {
+  data: PropTypes.array.isRequired
+}
 
-export default HelloUA
+export default App
